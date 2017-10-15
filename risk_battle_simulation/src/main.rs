e0df8d224b0db7fcdd17463dd4bd80112b3fbdd7
alex <salexln@gmail.com>
2017-10-15 22:00:18 +0300

    working on user input

--- risk_battle_simulation/src/main.rs
+++ risk_battle_simulation/src/main.rs
@@ -1,11 +1,13 @@
 
 extern crate rand;
 use rand::Rng;
+use std::env;
 
 
 
 fn main() {
-    print_random_number();
+    //print_random_number();
+    parse_args();
 }
 
 
@@ -14,5 +16,25 @@ fn print_random_number() {
 
     if rng.gen() {
         println!("random number is {}", rng.gen::<i32>());
+    }
+}
+
+fn parse_args() {
+    let args: Vec<String> = env::args().collect();
+
+    if args[1] == "--help" {
+        println!("--number-of-attackers - Number of soldies to attack");
+        println!("--number-of-defenders - Number of soldies to defend");
+    } else {
+        if args.len() == 5 {
+            if args[1] == "--number-of-attackers" && args[3] == "--number-of-defenders" {
+                println!("valid input");
+            } else {
+                println!("Invalid input");
+            }
+        } else {
+            println!("Invalid input");
+        }
+
     }
 }
